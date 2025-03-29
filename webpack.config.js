c81248ace04ab819aae7a9e1c86f5216ec7f27db
Ailafeka <ailafeka@gmail.com>
2025-03-29 17:53:44 +0800

    引入React 需要重新修正JS功能

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,24 @@ module.exports = {
       path: path.resolve(__dirname, "dist"), // ✅ 絕對路徑，指定輸出資料夾
       filename: "bundle.js", // ✅ 這裡應該只是檔案名稱，不要加 "/"
     },
+    module:{
+      rules:[
+        {
+          test:/.js$/,
+          exclude:/node_modules/,
+          use:{
+              loader:'babel-loader',
+              options:{
+                  presets:[
+                      '@babel/preset-react',
+                      '@babel/preset-env'
+                  ],  
+              },
+          },
+        },
+      ],
+
+    },
     devServer:{
             static:{
                 directory: path.join(__dirname,'dist'),
